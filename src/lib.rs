c29143c8642705c735a94389928a450e4b6722f0
taitep <taitep@taitep.se>
2024-08-30 19:45:23 +0200

    Add some documentation to crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,39 @@
+//! # `string_error_wrap`
+//! Crate for easily creating error types wrapping String.
+//!
+//! This can be extremely useful for dealing with libraries
+//! using String as an error type in some functions when you
+//! use something like anyhow.
+//!
+//! # Usage
+//! ```
+//! // generates error type
+//! string_error_wrap::wrapper!(SomeError);
+//!
+//! fn main() -> Result<(), SomeError> {
+//!     // other_function returns a Result<T, String>
+//!     // but that can be converted into Result<T, SomeError>
+//!     // allowing the use of ?
+//!     other_function().map_err(SomeError::from)?;
+//! }
+//! ```
+
+/// Generates a String-wrapping error type
+///
+/// # Usage
+/// ```
+/// string_error_wrap::wrapper!(NameOfErrorTypeYouWant);
+/// ```
+/// will define a struct that implements `Debug`, `Error` and `Clone`
+/// as well as `From<String>`.
+///
+/// All instances of this being used results in a struct that
+/// behaves exactly the same, but with the specified name.
+///
+/// The Display implementation will result in something like the following:
+/// `ErrorIdentifier: message` where message is the contained string.
 #[macro_export]
-macro_rules! wrapper {
+macro_rules! wrapper_gen {
     ($name:ident) => {
         #[derive(Debug, thiserror::Error, Clone)]
         struct $name(String);
